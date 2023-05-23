a28aff35e4f849e2f390e657ed68d51944a38d51
picalloj17 <picalloj17@gmail.com>
2023-05-22 21:04:11 -0400

    more fleshed out homepage with title and cards

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Header';
+import Body from './Body';
 
 function App() {
     return (
-        <div id = 'root'>
+        <>
             <Header />
-        </div>
+            
+        </>
     )
 }
 
